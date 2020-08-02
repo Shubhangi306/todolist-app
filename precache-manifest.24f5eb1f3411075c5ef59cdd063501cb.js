daf181e8a184644f3b9cb1e7bc616af9e102841c
Shubhangi306 <65853105+Shubhangi306@users.noreply.github.com>
2020-08-02 13:59:26 +0530

    Updates

---
+++ precache-manifest.24f5eb1f3411075c5ef59cdd063501cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33cd71c4ac3e9ca3dee525a538ba2ac4",
+    "url": "/todolist-app/index.html"
+  },
+  {
+    "revision": "ea1c066ab9ed97c7b99e",
+    "url": "/todolist-app/static/css/main.28d9d3e1.chunk.css"
+  },
+  {
+    "revision": "a7266c860e9e80021347",
+    "url": "/todolist-app/static/js/2.4ce40846.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-app/static/js/2.4ce40846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1c066ab9ed97c7b99e",
+    "url": "/todolist-app/static/js/main.ad9fccd9.chunk.js"
+  },
+  {
+    "revision": "969e2ac58ed3b1f6ff2a",
+    "url": "/todolist-app/static/js/runtime-main.00c80139.js"
+  }
+]);
